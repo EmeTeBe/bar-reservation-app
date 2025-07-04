cb1b4cd17ec0125b2ae0e213b2d01647a04e9e2b
EmeTeBe <bernimarozzi@gmail.com>
2025-07-04 16:54:15 -0300

    localStorage save

--- src/context/ReservationProvider.tsx
+++ src/context/ReservationProvider.tsx
@@ -1,21 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ReservationContext } from "./ReservationContext";
 import { type ReservationData } from "./ReservationContext";
 
+const LOCAL_STORAGE_KEY = "reservationData";
 
-export function ReservationProvider({children,}: {children: React.ReactNode;}) {
-  const [reservation, setReservationState] = useState({
-    people: "",
-    day: "",
-    time: "",
-    notes: "",
-    name: "",
-    phone: "",
+export function ReservationProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [reservation, setReservationState] = useState<ReservationData>(() => {
+    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return storedData
+      ? JSON.parse(storedData)
+      : {
+          people: "",
+          day: "",
+          time: "",
+          notes: "",
+          name: "",
+          phone: "",
+        };
   });
 
   const setReservation = (data: Partial<ReservationData>) => {
-    setReservationState((prev) => ({ ...prev, ...data }));
+    setReservationState((prev) => {
+      const newState = { ...prev, ...data };
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+      return newState;
+    });
   };
+
+  useEffect(() => {
+    setReservationState((prev) => ({ ...prev, setReservation }));
+  }, []);
 
   return (
     <ReservationContext.Provider value={{ ...reservation, setReservation }}>
